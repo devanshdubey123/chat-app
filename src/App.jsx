ddec695278f2d1b3f82c1337ccecb11fe8fff07b
HP <201b092@juetguna.in>
2023-07-11 19:28:11 +0530

    edit styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+//import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/signin">SignIn</Route>
-        <Route path="/">Home</Route>
-      </Switch>
-    </BrowserRouter>
+    <div>hello</div>
   );
 }
 
