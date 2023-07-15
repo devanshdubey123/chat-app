4e10633ad8c120baea1544b27fa7b9904f49472c
HP <201b092@juetguna.in>
2023-07-15 12:38:45 +0530

    private and public page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 //import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
+import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import { Switch,Route } from 'react-router';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>hello</div>
-  );
+    <Switch>
+      <Route path="/signin">
+  <SignIn />
+      </Route>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+      </Switch> 
+      );
 }
 
 export default App;
